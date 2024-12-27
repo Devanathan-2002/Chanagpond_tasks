dd268cc0bfeea4532be2e8bd2db3647378dca94b
Devanathan-2002 <devanathan2k2@gmail.com>
2024-12-27 09:23:16 +0530

    commit change

--- Python_Task/tasks/day_3.py
+++ Python_Task/tasks/day_3.py
@@ -1,20 +1,28 @@
-def calculate_circle(Radius,pi=3.14):
-    return pi*Radius*Radius
+# def calculate_circle(Radius,pi=3.14):
+#     return pi*Radius*Radius
 
 
 
-def main():
-    radius=int(input('Enter the value: '))
-    print('positional argument')
-    print(calculate_circle(radius,3))
-    print('First argument positional sec default')
-    print(calculate_circle(radius))
-    print('Keyword argument')
-    print(round(calculate_circle(Radius=radius,pi=3.14),1))
-    # print('first argument Keyword')
-    # print(calculate_circle(Radius=radius,pi=3.14))
-    print('first argument Keyword and sec default')
-    print(calculate_circle(Radius=radius))
+# def main():
+#     radius=int(input('Enter the value: '))
+#     print('positional argument')
+#     print(calculate_circle(radius,3))
+#     print('First argument positional sec default')
+#     print(calculate_circle(radius))
+#     print('Keyword argument')
+#     print(round(calculate_circle(Radius=radius,pi=3.14),1))
+#     # print('first argument Keyword')
+#     # print(calculate_circle(Radius=radius,pi=3.14))
+#     print('first argument Keyword and sec default')
+#     print(calculate_circle(Radius=radius))
 
-if __name__=='__main__':
-    main()
+# if __name__=='__main__':
+#     main()
+
+
+
+# def menu():
+#     Menu=['Dosa','Idly','Biriyani']
+#     choices=input('Enter the choices:\n ')
+
+# print(menu())
